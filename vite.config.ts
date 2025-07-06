c1c3732bae272c0752388921cb42e4a6adb8839a
robert04x <rbosman374@gmail.com>
2025-07-06 21:13:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-  rollupOptions: {
-    external: ['/App.tsx']
-    external: ["/index.css"]
-  }
-  }
-}})
+    rollupOptions: {
+      external: ['/App.tsx'], // fixed array syntax
+    },
+  },
+});
